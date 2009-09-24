7fe751e1d77fb7e7c1a90bfebacbe7075fdecd5a
Insider <insider42rus@yandex.ru>
2009-09-24 14:54:15 +0800

    SD2 updated to 1409

--- src/bindings/ScriptDev2/system/ScriptLoader.cpp
+++ src/bindings/ScriptDev2/system/ScriptLoader.cpp
@@ -244,6 +244,13 @@ extern void AddSC_winterspring();
 
 //northrend
 extern void AddSC_instance_azjol_nerub();                   //azjol-nerub
+extern void AddSC_boss_novos();                             //draktharon_keep
+extern void AddSC_boss_tharonja();
+extern void AddSC_boss_trollgore();
+extern void AddSC_boss_colossus();                          //gundrak
+extern void AddSC_boss_galdarah();
+extern void AddSC_boss_moorabi();
+extern void AddSC_boss_sladran();
 extern void AddSC_boss_anubrekhan();                        //naxxramas
 extern void AddSC_boss_four_horsemen();
 extern void AddSC_boss_faerlina();
@@ -269,12 +276,17 @@ extern void AddSC_boss_ionar();
 extern void AddSC_boss_loken();
 extern void AddSC_boss_volkhan();
 extern void AddSC_instance_halls_of_lightning();
+extern void AddSC_instance_ulduar();                        //ulduar
 extern void AddSC_npc_ulduar_teleporter();                  //Ulduar
 extern void AddSC_boss_ingvar();                            //utgarde_keep
 extern void AddSC_boss_keleseth();
 extern void AddSC_boss_skarvald_and_dalronn();
 extern void AddSC_instance_utgarde_keep();
 extern void AddSC_utgarde_keep();
+extern void AddSC_boss_gortok();                           //utgarde_pinnacle
+extern void AddSC_boss_skadi();
+extern void AddSC_boss_svala();
+extern void AddSC_boss_ymiron();
 extern void AddSC_instance_vault_of_archavon();
 extern void AddSC_boss_emalon();
 
@@ -607,6 +619,13 @@ void AddScripts()
 
     //northrend
     AddSC_instance_azjol_nerub();                           //azjol-nerub
+    AddSC_boss_novos();                                     //draktharon_keep
+    AddSC_boss_tharonja();
+    AddSC_boss_trollgore();
+    AddSC_boss_colossus();                                  //gundrak
+    AddSC_boss_galdarah();
+    AddSC_boss_moorabi();
+    AddSC_boss_sladran();
     AddSC_boss_anubrekhan();                                //naxxramas
     AddSC_boss_four_horsemen();
     AddSC_boss_faerlina();
@@ -632,12 +651,17 @@ void AddScripts()
     AddSC_boss_loken();
     AddSC_boss_volkhan();
     AddSC_instance_halls_of_lightning();
+    AddSC_instance_ulduar();                                //ulduar
     AddSC_npc_ulduar_teleporter();                          //Ulduar
     AddSC_boss_ingvar();                                    //utgarde_keep
     AddSC_boss_keleseth();
     AddSC_boss_skarvald_and_dalronn();
     AddSC_instance_utgarde_keep();
     AddSC_utgarde_keep();
+    AddSC_boss_gortok();                                    //utgarde_pinnacle
+    AddSC_boss_skadi();
+    AddSC_boss_svala();
+    AddSC_boss_ymiron();
     AddSC_instance_vault_of_archavon();
     AddSC_boss_emalon();
 
