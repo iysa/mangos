0f196df3b7f30de569ec7d8cf9c70a8c07a3e94d
Insider <insider42rus@yandex.ru>
2009-10-02 18:47:43 +0800

    SD2: Reverted [patch 11]

--- src/bindings/ScriptDev2/scripts/northrend/nexus/nexus/boss_anomalus.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/nexus/nexus/boss_anomalus.cpp
@@ -16,27 +16,12 @@
 
 /* ScriptData
 SDName: Boss_Anomalus
-SD%Complete: 
-SDComment: 
-SDCategory: The Nexus, The Nexus
+SD%Complete: 10%
+SDComment:
+SDCategory: Nexus
 EndScriptData */
 
 #include "precompiled.h"
-#include "def_nexus.h"
-
-//Spells
-#define SPELL_SPARK_N              47751
-#define SPELL_SPARK_H              57062
-#define SPELL_RIFT_SHIELD          47748
-#define SPELL_CHARGE_RIFT          47747 //Works wrong (affect players, not rifts)
-#define SPELL_CREATE_RIFT          47743 //Don't work, using WA
-#define SPELL_ARCANE_ATTRACTION    57063 //No idea, when it's used
-
-#define MOB_CRAZED_MANA_WRAITH              26746
-#define MOB_CHAOTIC_RIFT                    26918
-#define SPELL_CHAOTIC_ENERGY_BURST          47688
-#define SPELL_CHARGED_CHAOTIC_ENERGY_BURST  47737
-#define SPELL_ARCANEFORM                    48019 //Chaotic Rift visual
 
 enum
 {
@@ -45,52 +30,43 @@ enum
     SAY_SHIELD              = -1576008,
     SAY_KILL                = -1576009,
     SAY_DEATH               = -1576010,
+
+    SPELL_CREATE_RIFT       = 47743,
+    SPELL_CHARGE_RIFT       = 47747,
+    SPELL_RIFT_SHIELD       = 47748,
+
+    SPELL_SPARK             = 47751,
+    SPELL_SPARK_H           = 57062
 };
 
-float RiftLocation[6][3]=
-{
-    {652.64, -273.70, -8.75},
-    {634.45, -265.94, -8.44},
-    {620.73, -281.17, -9.02},
-    {626.10, -304.67, -9.44},
-    {639.87, -314.11, -9.49},
-    {651.72, -297.44, -9.37}
-};
+/*######
+## boss_anomalus
+######*/
 
 struct MANGOS_DLL_DECL boss_anomalusAI : public ScriptedAI
 {
     boss_anomalusAI(Creature* pCreature) : ScriptedAI(pCreature)
     {
-        pInstance = ((ScriptedInstance*)pCreature->GetInstanceData());
-        HeroicMode = pCreature->GetMap()->IsHeroic();
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        m_bIsHeroicMode = pCreature->GetMap()->IsHeroic();
         Reset();
     }
 
-    ScriptedInstance* pInstance;
-    bool HeroicMode;
+    ScriptedInstance* m_pInstance;
+    bool m_bIsHeroicMode;
 
-    uint8 Phase;
-    uint32 SPELL_SPARK_Timer;
-    uint32 SPELL_CREATE_RIFT_Timer;
-    uint64 ChaoticRiftGUID;
-
-    void Reset() 
+    void Reset()
     {
-        Phase = 0;
-        SPELL_SPARK_Timer = 5000;
-        SPELL_CREATE_RIFT_Timer = 25000;
-        ChaoticRiftGUID = 0;
-
-        if(pInstance)
-            pInstance->SetData(DATA_ANOMALUS_EVENT, NOT_STARTED);
     }
 
     void Aggro(Unit* pWho)
     {
         DoScriptText(SAY_AGGRO, m_creature);
+    }
 
-        if(pInstance)
-            pInstance->SetData(DATA_ANOMALUS_EVENT, IN_PROGRESS);
+    void JustDied(Unit* pKiller)
+    {
+        DoScriptText(SAY_DEATH, m_creature);
     }
 
     void KilledUnit(Unit* pVictim)
@@ -99,178 +75,18 @@ struct MANGOS_DLL_DECL boss_anomalusAI : public ScriptedAI
             DoScriptText(SAY_KILL, m_creature);
     }
 
-    void JustDied(Unit* pKiller)  
-    {
-        DoScriptText(SAY_DEATH, m_creature);
-
-        if (pInstance)
-            pInstance->SetData(DATA_ANOMALUS_EVENT, DONE);
-    }
-
-    void UpdateAI(const uint32 diff)
+    void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostilTarget() || !m_creature->getVictim())
             return;
-        
-        if (m_creature->HasAura(SPELL_RIFT_SHIELD))
-        {
-            Unit* Rift;
-            if (ChaoticRiftGUID)
-                Rift = Unit::GetUnit((*m_creature), ChaoticRiftGUID);
-            if (Rift && Rift->isDead())
-            {
-                m_creature->RemoveAurasDueToSpell(SPELL_RIFT_SHIELD);
-                ChaoticRiftGUID = 0;
-            }
-            return;
-        } else
-            ChaoticRiftGUID = 0;
-
-        if ((Phase == 0) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.75))
-        {
-            Phase = 1;
-            DoScriptText(SAY_SHIELD, m_creature);
-            DoCast(m_creature, SPELL_RIFT_SHIELD);
-
-            int tmp = rand()%(2);
-            Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
-            if (Rift)
-            {
-                //DoCast(Rift, SPELL_CHARGE_RIFT);
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    Rift->AI()->AttackStart(target);
-                ChaoticRiftGUID = Rift->GetGUID();
-                DoScriptText(SAY_RIFT, m_creature);
-            }
-        }
-
-        if ((Phase == 1) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.50))
-        {
-            Phase = 2;
-            DoScriptText(SAY_SHIELD, m_creature);
-            DoCast(m_creature, SPELL_RIFT_SHIELD);
-
-            int tmp = rand()%(2);
-            Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
-            if (Rift)
-            {
-                //DoCast(Rift, SPELL_CHARGE_RIFT);
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    Rift->AI()->AttackStart(target);
-                ChaoticRiftGUID = Rift->GetGUID();
-                DoScriptText(SAY_RIFT, m_creature);
-            }
-        }
-
-        if ((Phase == 2) && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.25))
-        {
-            Phase = 3;
-            DoScriptText(SAY_SHIELD, m_creature);
-            DoCast(m_creature, SPELL_RIFT_SHIELD);
-
-            int tmp = rand()%(2);
-            Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
-            if (Rift)
-            {
-                //DoCast(Rift, SPELL_CHARGE_RIFT);
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    Rift->AI()->AttackStart(target);
-                ChaoticRiftGUID = Rift->GetGUID();
-                DoScriptText(SAY_RIFT, m_creature);
-            }
-        }
-
-        if (SPELL_SPARK_Timer < diff)
-        {
-            if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                DoCast(target, HeroicMode ? SPELL_SPARK_H : SPELL_SPARK_N);
-            SPELL_SPARK_Timer = 5000;
-        }else SPELL_SPARK_Timer -=diff;
-
-        if (SPELL_CREATE_RIFT_Timer < diff)
-        {
-            DoScriptText(SAY_RIFT, m_creature);
-
-            int tmp = rand()%(2);
-            Creature* Rift = m_creature->SummonCreature(MOB_CHAOTIC_RIFT, RiftLocation[tmp][0], RiftLocation[tmp][1], RiftLocation[tmp][2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
-            if (Rift)
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    Rift->AI()->AttackStart(target);
-            SPELL_CREATE_RIFT_Timer = 25000;
-        }else SPELL_CREATE_RIFT_Timer -=diff;
 
         DoMeleeAttackIfReady();
     }
 };
 
-struct MANGOS_DLL_DECL mob_chaotic_riftAI : public Scripted_NoMovementAI
-{
-    mob_chaotic_riftAI(Creature* pCreature) : Scripted_NoMovementAI(pCreature)
-    {
-        pInstance = ((ScriptedInstance*)pCreature->GetInstanceData());
-        Reset();
-    }
-
-    ScriptedInstance* pInstance;
-
-    uint32 SPELL_CHAOTIC_ENERGY_BURST_Timer;
-    uint32 SUMMON_CRAZED_MANA_WRAITH_Timer;
-
-    void Reset()
-    {
-        SPELL_CHAOTIC_ENERGY_BURST_Timer = 1000;
-        SUMMON_CRAZED_MANA_WRAITH_Timer = 5000;
-        m_creature->SetDisplayId(25206); //For some reason in DB models for ally and horde are different.
-                                         //Model for ally (1126) does not show auras. Horde model works perfect.
-                                         //Set model to horde number
-        DoCast(m_creature, SPELL_ARCANEFORM, false);
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (!m_creature->SelectHostilTarget() || !m_creature->getVictim())
-            return;
-
-        if (SPELL_CHAOTIC_ENERGY_BURST_Timer < diff)
-        {
-            if (pInstance)
-            {
-                Unit* Anomalus = Unit::GetUnit((*m_creature), pInstance->GetData64(DATA_ANOMALUS));
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    if (Anomalus && Anomalus->HasAura(SPELL_RIFT_SHIELD))
-                        DoCast(target, SPELL_CHARGED_CHAOTIC_ENERGY_BURST);
-                    else
-                        DoCast(target, SPELL_CHAOTIC_ENERGY_BURST);
-                SPELL_CHAOTIC_ENERGY_BURST_Timer = 1000;
-            }
-        }else SPELL_CHAOTIC_ENERGY_BURST_Timer -=diff;
-
-        if (SUMMON_CRAZED_MANA_WRAITH_Timer < diff)
-        {
-            Creature* Wraith = m_creature->SummonCreature(MOB_CRAZED_MANA_WRAITH, m_creature->GetPositionX()+1, m_creature->GetPositionY()+1, m_creature->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 1000);
-            if (Wraith)
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    Wraith->AI()->AttackStart(target);
-            if (pInstance)
-            {
-                Unit* Anomalus = Unit::GetUnit((*m_creature), pInstance->GetData64(DATA_ANOMALUS));
-                if (Anomalus && Anomalus->HasAura(SPELL_RIFT_SHIELD))
-                    SUMMON_CRAZED_MANA_WRAITH_Timer = 5000;
-                else
-                    SUMMON_CRAZED_MANA_WRAITH_Timer = 10000;
-            }
-        }else SUMMON_CRAZED_MANA_WRAITH_Timer -=diff;
-    }
-};
-
-CreatureAI* GetAI_mob_chaotic_rift(Creature* pCreature)
-{
-    return new mob_chaotic_riftAI(pCreature);
-}
-
 CreatureAI* GetAI_boss_anomalus(Creature* pCreature)
 {
-    return new boss_anomalusAI (pCreature);
+    return new boss_anomalusAI(pCreature);
 }
 
 void AddSC_boss_anomalus()
@@ -278,12 +94,7 @@ void AddSC_boss_anomalus()
     Script *newscript;
 
     newscript = new Script;
-    newscript->Name="boss_anomalus";
+    newscript->Name = "boss_anomalus";
     newscript->GetAI = &GetAI_boss_anomalus;
-    newscript->RegisterSelf();
-    
-    newscript = new Script;
-    newscript->Name="mob_chaotic_rift";
-    newscript->GetAI = &GetAI_mob_chaotic_rift;
     newscript->RegisterSelf();
 }
--- src/bindings/ScriptDev2/scripts/northrend/nexus/nexus/boss_keristrasza.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/nexus/nexus/boss_keristrasza.cpp
@@ -16,202 +16,83 @@
 
 /* ScriptData
 SDName: Boss_Keristrasza
-SD%Complete: 
-SDComment: 
-SDCategory: The Nexus, The Nexus
+SD%Complete: 10%
+SDComment:
+SDCategory: Nexus
 EndScriptData */
 
 #include "precompiled.h"
-#include "def_nexus.h"
-
-//Spells
-#define SPELL_FROZEN_PRISON                             47854
-#define SPELL_TAIL_SWEEP                                50155
-#define SPELL_CRYSTAL_CHAINS                            50997
-#define SPELL_ENRAGE                                    8599
-#define SPELL_CRYSTALFIRE_BREATH_N                      48096
-#define SPELL_CRYSTALFIRE_BREATH_H                      57091
-#define SPELL_CRYSTALIZE                                48179
-#define SPELL_INTENSE_COLD                              48094
-#define SPELL_INTENSE_COLD_TRIGGERED                    48095
 
 enum
 {
-    CONTAINMENT_SPHERES         = 3,
-
     SAY_AGGRO                   = -1576016,
     SAY_CRYSTAL_NOVA            = -1576017,
     SAY_ENRAGE                  = -1576018,
     SAY_KILL                    = -1576019,
     SAY_DEATH                   = -1576020,
+
+    SPELL_CRYSTALFIRE_BREATH    = 48096,
+    SPELL_CRYSTALFIRE_BREATH_H  = 57091,
+
+    SPELL_CRYSTALLIZE           = 48179,
+
+    SPELL_CRYSTAL_CHAINS        = 50997,
+    SPELL_CRYSTAL_CHAINS_H      = 57050,
+
+    SPELL_TAIL_SWEEP            = 50155,
+    SPELL_INTENSE_COLD          = 48094,
+
+    SPELL_ENRAGE                = 8599
 };
+
+/*######
+## boss_keristrasza
+######*/
 
 struct MANGOS_DLL_DECL boss_keristraszaAI : public ScriptedAI
 {
     boss_keristraszaAI(Creature* pCreature) : ScriptedAI(pCreature)
     {
-        pInstance = ((ScriptedInstance*)pCreature->GetInstanceData());
-        HeroicMode = pCreature->GetMap()->IsHeroic();
+        m_pInstance = (ScriptedInstance*)pCreature->GetInstanceData();
+        m_bIsHeroicMode = pCreature->GetMap()->IsHeroic();
         Reset();
     }
 
-    ScriptedInstance* pInstance;
-    bool HeroicMode;
-
-    uint32 CRYSTALFIRE_BREATH_Timer;
-    uint32 CRYSTAL_CHAINS_CRYSTALIZE_Timer;
-    uint32 TAIL_SWEEP_Timer;
-    uint32 StartCombat_Timer;
-    bool StartCombat;
-    bool Enrage;
-    uint64 ContainmentSphereGUIDs[CONTAINMENT_SPHERES];
+    ScriptedInstance* m_pInstance;
+    bool m_bIsHeroicMode;
 
     void Reset()
     {
-        CRYSTALFIRE_BREATH_Timer = 14000;
-        CRYSTAL_CHAINS_CRYSTALIZE_Timer = HeroicMode ? 30000 : 11000;
-        TAIL_SWEEP_Timer = 5000;
-        Enrage = false;
-        StartCombat_Timer = 2000;
-        StartCombat = false;
-        memset(&ContainmentSphereGUIDs, 0, sizeof(ContainmentSphereGUIDs));
-
-        m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_STUNNED);
-
-        RemovePrison(CheckContainmentSpheres());
-
-        if (pInstance)
-            pInstance->SetData(DATA_KERISTRASZA_EVENT, NOT_STARTED);
     }
 
     void Aggro(Unit* pWho)
     {
         DoScriptText(SAY_AGGRO, m_creature);
-        DoCast(m_creature, SPELL_INTENSE_COLD);
-
-        if (pInstance)
-            pInstance->SetData(DATA_KERISTRASZA_EVENT, IN_PROGRESS);
     }
 
-    void JustDied(Unit* pKiller)  
+    void JustDied(Unit* pKiller)
     {
         DoScriptText(SAY_DEATH, m_creature);
-
-        if (pInstance)
-            pInstance->SetData(DATA_KERISTRASZA_EVENT, DONE);
     }
 
     void KilledUnit(Unit* pVictim)
     {
-        DoScriptText(SAY_KILL, m_creature);
+        if (urand(0, 1))
+            DoScriptText(SAY_KILL, m_creature);
     }
 
-    bool CheckContainmentSpheres(bool remove_prison = false)
+    void UpdateAI(const uint32 uiDiff)
     {
-        ContainmentSphereGUIDs[0] = pInstance->GetData64(ANOMALUS_CONTAINMET_SPHERE);
-        ContainmentSphereGUIDs[1] = pInstance->GetData64(ORMOROKS_CONTAINMET_SPHERE);
-        ContainmentSphereGUIDs[2] = pInstance->GetData64(TELESTRAS_CONTAINMET_SPHERE);
-
-        GameObject* ContainmentSpheres[CONTAINMENT_SPHERES];
-
-        for (uint8 i = 0; i < CONTAINMENT_SPHERES; i++)
-        {
-            ContainmentSpheres[i] = pInstance->instance->GetGameObject(ContainmentSphereGUIDs[i]);
-            if (!ContainmentSpheres[i])
-                return false;
-            if (ContainmentSpheres[i]->GetGoState() != GO_STATE_ACTIVE)
-                return false;
-        }
-        
-        if (remove_prison)
-            RemovePrison(true);
-        
-        return true;
-    }
-
-    void RemovePrison(bool remove)
-    {
-        if (remove)
-        {
-            m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-            if (m_creature->HasAura(SPELL_FROZEN_PRISON))
-            {
-                StartCombat = true;
-                m_creature->RemoveAurasDueToSpell(SPELL_FROZEN_PRISON);
-            }
-        }
-        else
-        {
-            m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-            m_creature->CastSpell(m_creature, SPELL_FROZEN_PRISON, false);
-        }
-    }
-
-    void UpdateAI(const uint32 diff)
-    {
-        if (StartCombat)
-            if (StartCombat_Timer < diff)
-            {
-                StartCombat = false;
-                m_creature->SetInCombatWithZone();
-            }else StartCombat_Timer -=diff;
-        
         if (!m_creature->SelectHostilTarget() || !m_creature->getVictim())
             return;
-          
-        if (!Enrage && (m_creature->GetHealth() < m_creature->GetMaxHealth() * 0.25))
-        {
-            DoScriptText(SAY_ENRAGE , m_creature);
-            DoCast(m_creature, SPELL_ENRAGE);
-            Enrage = true;
-        }
-
-        if (CRYSTALFIRE_BREATH_Timer < diff)
-        {
-            DoCast(m_creature->getVictim(), HeroicMode ? SPELL_CRYSTALFIRE_BREATH_H : SPELL_CRYSTALFIRE_BREATH_N);
-            CRYSTALFIRE_BREATH_Timer = 14000;
-        }else CRYSTALFIRE_BREATH_Timer -=diff;
-
-        if (TAIL_SWEEP_Timer < diff)
-        {
-            DoCast(m_creature, SPELL_TAIL_SWEEP);
-            TAIL_SWEEP_Timer = 5000;
-        }else TAIL_SWEEP_Timer -=diff;
-
-        if (CRYSTAL_CHAINS_CRYSTALIZE_Timer < diff)
-        {
-            DoScriptText(SAY_CRYSTAL_NOVA , m_creature);
-            if (HeroicMode)
-                DoCast(m_creature, SPELL_CRYSTALIZE);
-            else
-                if (Unit* target = SelectUnit(SELECT_TARGET_RANDOM, 0))
-                    DoCast(target, SPELL_CRYSTAL_CHAINS);
-            CRYSTAL_CHAINS_CRYSTALIZE_Timer = HeroicMode ? 30000 : 11000;
-        }else CRYSTAL_CHAINS_CRYSTALIZE_Timer -= diff;
 
         DoMeleeAttackIfReady();
     }
 };
 
-bool GOHello_containment_sphere(Player* pPlayer, GameObject* pGO)
-{
-    ScriptedInstance* pInstance = (ScriptedInstance*)pGO->GetInstanceData();
-
-    Unit* Keristrasza = Unit::GetUnit(*pGO, pInstance->GetData64(DATA_KERISTRASZA));
-    if (Keristrasza && Keristrasza->isAlive())
-    {
-        // maybe these are hacks :(
-        pGO->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_UNK1);
-        pGO->SetGoState(GO_STATE_ACTIVE);
-
-        ((boss_keristraszaAI*)((Creature*)Keristrasza)->AI())->CheckContainmentSpheres(true);
-    }
-    return true;
-}
-
 CreatureAI* GetAI_boss_keristrasza(Creature* pCreature)
 {
-    return new boss_keristraszaAI (pCreature);
+    return new boss_keristraszaAI(pCreature);
 }
 
 void AddSC_boss_keristrasza()
@@ -221,10 +102,5 @@ void AddSC_boss_keristrasza()
     newscript = new Script;
     newscript->Name = "boss_keristrasza";
     newscript->GetAI = &GetAI_boss_keristrasza;
-    newscript->RegisterSelf();
-
-    newscript = new Script;
-    newscript->Name = "containment_sphere";
-    newscript->pGOHello = &GOHello_containment_sphere;
     newscript->RegisterSelf();
 }
